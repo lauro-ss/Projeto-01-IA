dc5a11057f5512eaecd2d4312b5034d58c5f8057
Lauro Santana Silva <bestlaurobr@gmail.com>
2022-08-27 16:23:58 -0300

    tt

--- src/App.java
+++ src/App.java
@@ -5,10 +5,12 @@ public class App {
         
         VeIA ia = new VeIA();
         VeIA ia2 = new VeIA();
-        System.out.println(ia.jogue(".........", ".........".replace(".", "").length()));
-        System.out.println(ia2.jogue("....O....", "....O....".replace(".", "").length()));
-        System.out.println(ia.jogue("X...O....", "X...O....".replace(".", "").length()));
-        System.out.println(ia2.jogue("X.O.O....", "X.O.O....".replace(".", "").length()));
+        System.out.println(ia.jogue("....O....", "....O....".replace(".", "").length()));
+        System.out.println(ia.jogue("X.O.O....", "X.O.O....".replace(".", "").length()));
+        System.out.println(ia.jogue("X.OOO.X..", "X.OOO.X..".replace(".", "").length()));
+        //System.out.println(ia.jogue(".........", ".........".replace(".", "").length()));
+        //System.out.println(ia2.jogue("....O....", "....O....".replace(".", "").length()));
+        //System.out.println(ia2.jogue("X.O.O....", "X.O.O....".replace(".", "").length()));
         //System.out.println(ia.jogue("XOX.O.O..", "XOX.O.O..".replace(".", "").length()));   
         //mapa_vitoria[random.nextInt(c)].printMatriz();
         //mapa_empate[random.nextInt(z)].printMatriz();
